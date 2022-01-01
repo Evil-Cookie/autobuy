791dfeed8c44df5ffc39aa7e83f9d4f9138fc899
evil-coockie <nikita.s.mozhaev@gmail.com>
2022-01-01 10:11:22 +0300

    final

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from colorama import Fore, Back, Style
 import pyautogui
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys 
 
+
+
+pyautogui.alert('Program is started... Waiting 5 secounds')
 
 print(Fore.GREEN + "Starting...")
 
-print(pyautogui.position())
-pyautogui.scroll(100)
+print(Fore.GREEN + "Waiting...")
 
+time.sleep(5)
+
+print(Fore.BLUE + "Buying....")
+
+print(pyautogui.position())
+
+while True:
+    pyautogui.click()
--- re
+++ re
@@ -4,3 +4,4 @@ PyScreeze==0.1.28
 PyMsgBox==1.0.9
 pytweening==1.0.4
 PyInstaller==4.7
+selenium==4.1.0
